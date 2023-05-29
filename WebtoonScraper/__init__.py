644533ac24d34ecd8e8c260ffac556974910c348
ilotoki0804 <ilotoki0804@gmail.com>
2023-05-29 18:03:24 +0900

    bugfix

--- WebtoonScraper/__init__.py
+++ WebtoonScraper/__init__.py
@@ -1,3 +1,11 @@
 import WebtoonScraper.getsoup as getsoup
-from WebtoonScraper.scraper import NaverWebtoonScraper
+from WebtoonScraper.scraper import NaverWebtoonScraper, WebtoonsScraper
 from WebtoonScraper.foldermanagement import WebtoonFolderManagement
+
+if __name__ == '__main__':
+    print('Testing codes.')
+    webtoon = NaverWebtoonScraper()
+    webtoon.get_webtoons(766648)
+
+    webtoons = WebtoonsScraper()
+    webtoons.get_webtoons(5291, 1435)
--- tests/tests.py
+++
@@ -1,8 +0,0 @@
-# from WebtoonScraper.WebtoonScraper import *
-# from WebtoonScraper import *
-
-webtoon = NaverWebtoonScraper()
-webtoon.get_webtoons(766648)
-
-webtoons = WebtoonsScraper()
-webtoons.get_webtoons(5291, 1435)
