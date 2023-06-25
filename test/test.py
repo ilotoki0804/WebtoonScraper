4ed2c02ceeb77ed32a1d54838df58ac44f8302bb
ilotoki0804 <ilotoki0804@gmail.com>
2023-06-25 21:34:04 +0900

    만화경 테스트 추가

--- test/test.py
+++ test/test.py
@@ -6,3 +6,4 @@ if __name__ == "__main__":
     # wt.get_webtoon(597061, wt.NAVER_POST) # 19803452
     wt.get_webtoon(597061, wt.NAVER_POST, member_no=19803452) # 19803452
     wt.get_webtoon(1007888, wt.BU, cookie='')
+    wt.get_webtoon(146, wt.T)
