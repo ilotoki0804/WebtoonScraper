430c04f834dc2029c217b65e3e8298c681a3f778
ilotoki0804 <ilotoki0804@gmail.com>
2023-07-30 18:31:30 +0900

    get_webtoon_data pyjsparser 대신 regex를 사용하는 방향으로 변화

--- WebtoonScraper/L_LezhinComicsScraper.py
+++ WebtoonScraper/L_LezhinComicsScraper.py
@@ -54,7 +54,7 @@ class LezhinComicsScraper(Scraper):
         return alt_webtoon_dir
 
     @alru_cache(maxsize=4)
-    async def get_webtoon_data(self, titleid: str):
+    async def old_get_webtoon_data(self, titleid: str):
         """Default titleid is titleid_str, and default episode_id is episode_id_str, which is displayed to users."""
         soup = await self.get_internet('soup', f'{self.BASE_URL}/{titleid}', )
         if soup.select_one('meta[property="og:title"]') == "404 - 레진코믹스":
@@ -117,6 +117,97 @@ class LezhinComicsScraper(Scraper):
         return {'title': title, 'webtoon_thumbnail': thumbnail_url,
                 'episode_ids': episode_id_strings[::-1], 'subtitles': subtitles[::-1],
                 'episode_id_ints': episode_id_ints[::-1], 'infomation_chars': infomation_chars[::-1],
+                'titleid_str': titleid, 'titleid_int': titleid_int, 'is_shuffled': is_shuffled, }
+
+    @alru_cache(maxsize=4)
+    async def get_webtoon_data(self, titleid: str, get_paid_episode: bool = False, get_unusable_episode: bool = False):
+        """Default titleid is titleid_str, and default episode_id is episode_id_str, which is displayed to users."""
+        soup = await self.get_internet('soup', f'{self.BASE_URL}/{titleid}', )
+        # print(soup.select_one('meta[property="og:title"]').content, soup)
+        if soup.select_one('meta[property="og:title"]').content in {"404 - 레진코믹스", ""}:
+            raise ValueError(f'Invalid {titleid = }')
+
+        title = soup.select_one("h2.comicInfo__title").text
+        thumbnail_url = soup.select_one('meta[property="og:image"]')["content"]
+
+        webtoon_raw_data = soup.select('script')[5]
+
+        if "src" in webtoon_raw_data.attrs:
+            raise ValueError(f'Invalid {titleid = }.')
+
+        try:
+            product_start = re.search(r"__LZ_PRODUCT__ *= *{\n *productType: *'comic',\n *product: *", webtoon_raw_data.text).end()
+            product_end, departure_start = re.search(",\n *departure: *'',\n *all: *", webtoon_raw_data.text).span()
+            # departure_end = re.search(",\n *prefree", webtoon_raw_data.text).start()
+
+            product = json.loads(webtoon_raw_data.text[product_start:product_end])
+            # # departure는 product["episodes"]와 완전히 같기 때문에 굳이 사용할 이유가 없다.
+            # departure = json.loads(webtoon_raw_data.text[departure_start:departure_end])
+        except (AttributeError, json.JSONDecodeError) as e:
+            raise ValueError("JavaScript cannot be parsed by regex; because it's not regular language. "
+                             "But sometimes, people have to compromise with effeciency. "
+                             "That's why this failed. call developer to fix this problem "
+                             "and Use `old_get_webtoon_data` instead while developer fix this.") from e
+
+        title = product["display"]["title"]
+        # titleid_str = product["alias"]
+        titleid_int = product["id"]
+        # is_adult = product["isAdult"]
+        if "metadata" in product:
+            metadata = product["metadata"]
+            is_shuffled = metadata["imageShuffle"] if "imageShuffle" in metadata else False
+        else:
+            is_shuffled = False
+
+        # departure는 product['episodes']와 동일하다.
+        episode_id_ints: list[int] = []
+        episode_id_strs: list[str] = []
+        subtitles: list[str] = []
+        episode_type_chars: list[str] = []
+        display_names: list[str] = []
+        # for episode in departure:
+        for episode in product["episodes"]:
+            # print(episode)
+            is_episode_expired = episode["properties"]["expired"]
+            is_episode_not_for_sale = episode["properties"]["notForSale"]
+            is_episode_free = "freedAt" in episode
+
+            if is_episode_free == episode["coin"]:
+                # bool(episode["coin"])도 `"freedAt" in episode` 동일한 역할을 할 것으로 기대된다.
+                # 가설이 맞는지 확인하고, 어떤 추측이 더 알맞는 방법인지를 확인한다.
+                # 적당히 확인하고 나서는 없애도 무관하다.
+                logging.warning(
+                    '`"freedAt" in episode == (not episode["coin"])` turned out to be false.' "[Developing purpose message]")
+                logging.warning(f'{is_episode_free = }, {bool(episode["coin"]) = }')
+
+            if (is_episode_expired or is_episode_not_for_sale) and not get_unusable_episode:
+                logging.warning(
+                    f"episode {episode['display']['title']} is not usable because it's marked as "
+                    + "expired." * is_episode_expired
+                    + "And it's " * (is_episode_expired and is_episode_not_for_sale)
+                    + "not-for-sale." * is_episode_not_for_sale
+                )
+                continue
+            # logging.warning((is_episode_free, get_paid_episode))
+            if not is_episode_free and not get_paid_episode:
+                logging.warning(
+                    f"episode {episode['display']['title']} is not free so it'll be skipped. " "If you want to get data about paid episode too, make parameter " "`get_unusable_episode` to True.")
+                continue
+
+            episode_id_ints.append(episode["id"])
+            episode_id_strs.append(episode["name"])
+            subtitles.append(episode["display"]["title"])
+            episode_type_chars.append(episode["display"]["type"])
+            display_names.append(episode["display"]["displayName"])
+            # episode_id_strs와 거의 같지만 특별편인 경우 'x1' 등으로 표시되는 episode_id_strs과는 달리
+            # '공지'와 같은 글자로 나타나며 에피소드 위 작은 글씨를 의미한다. 스크래핑과는 큰 관련이 없는 자료이다.\
+
+            # episode_ids[::-1], episode_id_strs[::-1], subtitles[::-1], episode_type_chars[::-1], display_names[::-1]
+            # title, titleid_str, titleid_int, is_adult, is_shuffled
+
+        return {'title': title, 'webtoon_thumbnail': thumbnail_url,
+                'episode_ids': episode_id_strs[::-1], 'subtitles': subtitles[::-1],
+                'episode_id_ints': episode_id_ints[::-1], 'infomation_chars': episode_type_chars[::-1],
                 'titleid_str': titleid, 'titleid_int': titleid_int, 'is_shuffled': is_shuffled, }
 
     async def get_title(self, titleid):
