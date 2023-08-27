ca12b1dd61394f9aa1e5506e212546c05252deb4
ilotoki0804 <ilotoki0804@gmail.com>
2023-08-27 17:59:31 +0900

    빌드 파일 수정

--- build.py
+++ build.py
@@ -3,11 +3,13 @@ import shutil
 import subprocess
 from pathlib import Path
 
+PYTHON_PATH = Path('python_path.txt').read_text(encoding='utf-8')
+
 shutil.rmtree('dist')
-os.system('python setup.py sdist bdist_wheel')
+os.system(f'{PYTHON_PATH}python.exe setup.py sdist bdist_wheel')
 whl_file_name = os.listdir('dist')[0]
-os.system(f'pip install --force-reinstall dist/{whl_file_name}')  # --user를 추가하면 오류가 덜 날 수도 있음
+os.system(f'{PYTHON_PATH}python.exe -m pip install --force-reinstall dist/{whl_file_name}')  # --user를 추가하면 오류가 덜 날 수도 있음
 if input('Submit changes? (y or not)') in ('y', 'Y', 'ㅛ'):
     token = Path('token.txt').read_text(encoding='utf-8')
     subprocess.run(["twine", "upload", "-u", '__token__', "-p", token, "dist/*"])
-    os.system('pip show webtoonscraper')
+    os.system(f'{PYTHON_PATH}python.exe -m pip show webtoonscraper')
