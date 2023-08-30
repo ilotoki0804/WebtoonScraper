64a70cdaadc7cde6748017032af3e6aeb552ef10
ilotoki0804 <ilotoki0804@gmail.com>
2023-08-30 22:39:40 +0900

    check_directory_state추가

--- WebtoonScraper/a_folder_merger.py
+++ WebtoonScraper/a_folder_merger.py
@@ -1,13 +1,27 @@
 """This module provides FolderMerger class."""
 
 from __future__ import annotations
+from http.client import NOT_IMPLEMENTED
 import os
 import shutil
 import re
 from collections import defaultdict
 from pathlib import Path
 import logging
+from textwrap import indent
+from typing_extensions import Literal, Final
+from xmlrpc.client import NOT_WELLFORMED_ERROR
 
+from sqlalchemy import literal
+
+DEFAULT_STATE: Final = 'default_state'
+MERGED: Final = 'merged'
+UNIFIED: Final = 'unified'
+NOT_MATCHED: Final = 'not_matched'
+
+DIRECTORY_STATES = Literal['default_state', 'merged', 'unified', 'not_matched']
+
+PathOrStr = str | Path
 
 class FolderMerger:
     """웹툰 뷰어 앱에서 정주행하기 좋도록 회차들을 단위에 따라 묶습니다."""
@@ -116,11 +130,10 @@ class FolderMerger:
             merge_last_bundle (bool, optional): _description_. Defaults to True.
 
         Raises:
-            NotImplementedError: 만약 base_webtoon_dir와 alt_webtoon_dir가 같은 경우에 생깁니다. merge_webtoons_in_directory를 대신 이용하세요.
+            AssertionError: 만약 base_webtoon_dir와 alt_webtoon_dir가 같은 경우에 생깁니다. merge_webtoons_in_directory를 대신 이용하세요.
         """
         # base_webtoon_dir와 alt_webtoon_dir가 같으면 안됨!
-        if base_webtoon_dir == alt_webtoon_dir:
-            raise NotImplementedError('base_webtoon_dir and alt_webtoon_dir cannot be same. Use merge_webtoon_episodes instead.')
+        assert base_webtoon_dir != alt_webtoon_dir, 'base_webtoon_dir and alt_webtoon_dir cannot be same. Use merge_webtoon_episodes instead.'
 
         alt_webtoon_dir.mkdir(parents=True, exist_ok=True)
 
@@ -128,7 +141,7 @@ class FolderMerger:
         self._move_thumbnail(base_webtoon_dir, alt_webtoon_dir)
 
         # 에피소드를 분해해 base_webtoon_dir에 형식에 맞추어 넣음
-        if not self._is_unified(base_webtoon_dir):
+        if not self.is_unified(base_webtoon_dir):
             self._unify_webtoon(base_webtoon_dir)
 
         # episode_bundle이 1인 경우 revert_to_original_download_state 수행
@@ -160,7 +173,7 @@ class FolderMerger:
                 shutil.move(image_dir, images_dir)
 
     def _move_thumbnail(self, base_webtoon_dir, alt_webtoon_dir):
-        if self._is_unified(base_webtoon_dir):
+        if self.is_unified(base_webtoon_dir):
             logging.debug('Webtoon look unified already, so _move_thumbnail is skipped.')
             return
         for episode_or_thumbnail in os.listdir(base_webtoon_dir):
@@ -227,13 +240,131 @@ class FolderMerger:
         image_no, image_extension = image_name.split('.')[0], image_name.split('.')[-1]
         return f'{episode_split[1]}.{image_no}. {episode_split[2]}.{image_extension}'
 
-    def _is_unified(self, directory) -> bool:
-        episodes_or_images = os.listdir(directory)
+    def is_unified(self, directory: PathOrStr) -> bool:
+        episodes_or_images: list[str] = os.listdir(directory)
+        webtoon_is_merged = re.compile(r'.+[.](jpg|jpeg|png|webp|gif)$', re.I)
+        # webtoon_is_merged = re.compile(r'^.+\..{3,4}$', re.I)
         number_of_images = sum(
-            1 if re.match(r'.+[.](jpg|jpeg|png)$', episode_or_image, re.I) else 0
+            1 if webtoon_is_merged.match(episode_or_image) else 0
             for episode_or_image in episodes_or_images
         )
-        return number_of_images not in [1, 0]
+        return number_of_images not in {1, 0}
+
+    def fast_check_directory_state(self, directory: PathOrStr) -> DIRECTORY_STATES:
+        """
+        detailed_check_directory_state는 사용자용으로, 정확한 오류 메시지와 체크를 목적으로 하지만,
+        이 함수는 프로그램 내에서 assertion을 목적으로, 빠른 체크를 목적으로 합니다.
+        주의: detailed_check_directory_state를 수정할 때는 fast_check_directory_state도 수정해야 합니다.
+        """
+        episodes_or_images: list[str] = os.listdir(directory)
+
+        if len(episodes_or_images) <= 1:
+            logging.warning(f"It looks like the directory is {'almost' if len(episodes_or_images) else ''} empty. "
+                            "Check your directory again.")
+
+        # 확장자를 직접 이용하지 않기 때문에 re.I가 필요 없다.
+        webtoon_is_unified = re.compile(r'\d{4}[.]\d{3}[.].+[.][a-zA-Z]{3,4}$')
+        webtoon_is_merged = re.compile(r'\d{4}~\d{4}')
+        webtoon_is_default_state = re.compile(r'^\d{4}\. .+$')
+        it_is_not_webtoon_rather_webtoons_folder = re.compile(r'^.+[(].+[)]([(]merged[)])?$')
+
+        directory_states: list[DIRECTORY_STATES] = [UNIFIED, MERGED, DEFAULT_STATE]
+        regexes_to_use = [webtoon_is_unified, webtoon_is_merged, webtoon_is_default_state, it_is_not_webtoon_rather_webtoons_folder]
+        for state_name, regex in zip(directory_states, regexes_to_use):
+            # 매치되지 '않은' 것의 개수를 세는 것을 주의!!!
+            number_of_images_NOT_matched = sum(
+                0 if regex.match(episode_or_image) else 1
+                for episode_or_image in episodes_or_images
+            )
+
+            if number_of_images_NOT_matched <= 1:
+                return state_name
+
+        return NOT_MATCHED
+
+    def detailed_check_directory_state(self, directory) -> DIRECTORY_STATES:
+        """
+        디렉토리가 merge된 상태인지 기본 상태(default_state)인지, unified된 상태인지, 아니면 일치하는 게 없는지 확인합니다.
+        사용자용으로, 상세한 경고 메시지와 정확한 체크를 제공합니다. 단, 중요한 프로그램을 처음 가동할 때는 사용할 수 있습니다.
+        주의: fast_check_directory_state를 수정할 때는 detailed_check_directory_state도 수정해야 합니다.
+        """
+        episodes_or_images: list[str] = os.listdir(directory)
+
+        # 확장자를 직접 이용하지 않기 때문에 re.I가 필요 없다.
+        webtoon_is_unified = re.compile(r'\d{4}[.]\d{3}[.].+[.][a-zA-Z]{3,4}$')
+        webtoon_is_merged = re.compile(r'\d{4}~\d{4}')
+        webtoon_is_default_state = re.compile(r'^\d{4}\. .+$')
+        it_is_not_webtoon_rather_webtoons_folder = re.compile(r'^.+[(].+[)]([(]merged[)])?$')
+
+        # # less error-prone but lossely checking regexes
+        # webtoon_is_unified = re.compile(r'\d+[.]\d+[.].+[.].{3,4}$')
+        # webtoon_is_merged = re.compile(r'\d+~\d+')
+        # webtoon_is_default_state = re.compile(r'^\d{4}\. .+$')  # unified와 구별하려면 space가 필수이다.
+
+        # webtoon_is_unified와 webtoon_is_default_state가 동시에 True라면 unified로 결과를 출력하는 것이 맞다.
+        # webtoon_is_unified가 더 엄격한 규칙을 가지고 있기 때문이다.
+        # webtoon_is_merged는 워낙 특이해서 false positive의 확률이 거의 없다.
+
+        directory_number_of_not_matched: list[int] = []
+        directory_bool_states: list[bool] = []
+        directory_state_names: list[DIRECTORY_STATES | Literal['webtoons_folder']] = []
+
+        directory_states: list[DIRECTORY_STATES | Literal['webtoons_folder']] = [UNIFIED, MERGED, DEFAULT_STATE, 'webtoons_folder']
+        regexes_to_use = [webtoon_is_unified, webtoon_is_merged, webtoon_is_default_state, it_is_not_webtoon_rather_webtoons_folder]
+        for state_name, regex in zip(directory_states, regexes_to_use):
+            # 매치되지 '않은' 것의 개수를 세는 것을 주의!!!
+            number_of_images_NOT_matched = sum(
+                0 if regex.match(episode_or_image) else 1
+                for episode_or_image in episodes_or_images
+            )
+
+            directory_number_of_not_matched.append(number_of_images_NOT_matched)
+            directory_bool_states.append(number_of_images_NOT_matched <= 1)
+            directory_state_names.append(state_name)
+
+        if directory_bool_states[directory_states.index('webtoons_folder')]:
+            logging.warning('This directory is looks like a webtoons_folder. Check your folder again.')
+            if sum(directory_bool_states) == 1:
+                return NOT_MATCHED
+        webtons_folder_index = directory_states.index('webtoons_folder')
+        del (directory_number_of_not_matched[webtons_folder_index],
+             directory_bool_states[webtons_folder_index],
+             directory_state_names[webtons_folder_index])
+
+        match sum(directory_bool_states):  # 3.10 이상만 실행 가능.
+            case 0:
+                logging.warning('This directory is nether unified, merged nor default_state. '
+                                "Probably the directory is not a webtoon folder.")
+                return NOT_MATCHED
+
+            case 1:
+                state_name = directory_state_names[directory_bool_states.index(True)]
+                assert state_name != 'webtoons_folder'
+                return state_name
+
+            case 2:
+                unmatched_directory_state_index = directory_bool_states.index(False)
+                del (directory_number_of_not_matched[unmatched_directory_state_index],
+                     directory_bool_states[unmatched_directory_state_index],
+                     directory_state_names[unmatched_directory_state_index])
+                logging.warning(f'This directory is either {directory_state_names[0]} and {directory_state_names[1]}. '
+                                "Probably there's a conflict or ")
+
+            case _:
+                match len(episodes_or_images):
+                    case 0:
+                        logging.warning('This directory is either unified, merged, and default_state. '
+                                        "It's because directory is empty.")
+                    case 1:
+                        logging.warning('This directory is either unified, merged, and default_state. '
+                                        "It's because directory only contains a single file, like thumbnail or a webtoon folder.")
+                    case _:
+                        logging.warning('This directory is either unified, merged, and default_state. '
+                                        "Maybe it's because directory is not a webtoon folder.")
+
+                return NOT_MATCHED
+
+        return NOT_MATCHED
 
     ############### RESTORE FUNCTIONALITY ###############
 
@@ -249,7 +380,7 @@ class FolderMerger:
         temp_thumbnail_path.mkdir(parents=True)
         self._move_thumbnail(directory, temp_thumbnail_path)
 
-        if not self._is_unified(directory):
+        if not self.is_unified(directory):
             # self._unify_webtoon(directory)
             directories = os.listdir(directory)
             directories = (
