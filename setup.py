09285a6ca449193c5d6407a31bfafab3616b43fb
ilotoki0804 <ilotoki0804@gmail.com>
2023-09-10 04:46:26 +0900

    __version__을 string으로 사용, setup.py 수정

--- setup.py
+++ setup.py
@@ -6,15 +6,19 @@ import WebtoonScraper
 long_description = '이 설명은 최신 버전이 아닐 수 있습니다. 만약 최신 버전을 확인하고 싶으시다면 [여기](https://github.com/ilotoki0804/WebtoonScraper)를 참고하세요.\n'
 long_description += Path('README.md').read_text(encoding='utf-8')
 # 사진 대체
-repl = r'[\g<1>](https://raw.githubusercontent.com/ilotoki0804/WebtoonScraper/master/\g<2>)'
-long_description = re.sub(r'[[](.*?)[]][(]((?:images|docs).*?)[)]', repl, long_description)
+repl = r'[\g<description>](https://raw.githubusercontent.com/ilotoki0804/WebtoonScraper/master/\g<path>)'
+long_description = re.sub(r'[[](?P<description>.*?)[]][(](..\/)*(?P<path>(?:images|docs).*?)[)]',
+                          repl, long_description)
 
 version = WebtoonScraper.__version__
+
+general_requirements = [line for line in Path('requirements.txt').read_text(encoding='utf-8').split()
+                        if line[0] == '#']
 
 if __name__ == '__main__':
     setup(
         name='WebtoonScraper',
-        version='.'.join(map(str, version)),
+        version=version,
         description='Scraping webtoons and some utils for it',
         author='ilotoki0804',
         author_email='ilotoki0804@gmail.com',
@@ -22,7 +26,7 @@ if __name__ == '__main__':
         long_description_content_type='text/markdown',
         license='MIT',
         url='https://github.com/ilotoki0804/WebtoonScraper',
-        install_requires=Path('requirements.txt').read_text(encoding='utf-8').split(),
+        install_requires=general_requirements,
         packages=find_packages(exclude=[]),
         keywords=['Webtoon', 'Webtoon Scraper', 'Naver Webtoon', 'Webtoon Downloader', 'Download Webtoon'],
         python_requires='>=3.10',
