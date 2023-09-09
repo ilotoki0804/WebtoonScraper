20111d70ff156055f0faed33e04d5a59d9ed50f8
ilotoki0804 <ilotoki0804@gmail.com>
2023-09-09 19:49:44 +0900

    requests_utils 버전명 명시

--- build.py
+++ build.py
@@ -8,7 +8,7 @@ PYTHON_PATH = Path('python_path.txt').read_text(encoding='utf-8')
 shutil.rmtree('dist')
 os.system(f'{PYTHON_PATH}python.exe setup.py sdist bdist_wheel')
 whl_file_name = os.listdir('dist')[0]
-os.system(f'{PYTHON_PATH}python.exe -m pip install --force-reinstall dist/{whl_file_name}')  # --user를 추가하면 오류가 덜 날 수도 있음
+os.system(f'{PYTHON_PATH}python.exe -m pip install --force-reinstall dist/{whl_file_name}')
 if input('Submit changes? (y or not)') in ('y', 'Y', 'ㅛ'):
     token = Path('token.txt').read_text(encoding='utf-8')
     subprocess.run(["twine", "upload", "-u", '__token__', "-p", token, "dist/*"])
