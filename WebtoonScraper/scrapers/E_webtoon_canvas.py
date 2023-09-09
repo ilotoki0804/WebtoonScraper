b7457d4d4fd7d33ba7e2b1612763790e80b27d98
ilotoki0804 <ilotoki0804@gmail.com>
2023-09-09 18:59:40 +0900

    캔버스 v2 스타일 적용

--- WebtoonScraper/scrapers/E_webtoon_canvas.py
+++ WebtoonScraper/scrapers/E_webtoon_canvas.py
@@ -9,11 +9,5 @@ else:
 
 class WebtoonCanvasScraper(WebtoonOriginalsScraper):
     '''Scrape webtoons from Webtoon Originals.'''
-    def __init__(self, pbar_independent=False):
-        super().__init__(pbar_independent)
-        self.BASE_URL = 'https://www.webtoons.com/en/challenge/meme-girls'
-
-
-if __name__ == '__main__':
-    wt = WebtoonCanvasScraper()
-    wt.download_one_webtoon(263735)  # Spookman
+    BASE_URL = 'https://www.webtoons.com/en/challenge/meme-girls'
+    TEST_WEBTOON_ID = 263735  # Spookman
