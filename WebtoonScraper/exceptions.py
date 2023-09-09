d4aeecdcb5901d2d9d02e4d2c6d8e52775807b34
ilotoki0804 <ilotoki0804@gmail.com>
2023-09-10 04:59:56 +0900

    InvalidWebtoonId exception 추가 (사용될지는 불확실)

--- WebtoonScraper/exceptions.py
+++ WebtoonScraper/exceptions.py
@@ -3,3 +3,7 @@
 
 class DirectoryStateUnmatched(ValueError):
     """directory state recieved from check_directory_state is not wanted."""
+
+
+class InvalidWebtoonId(ValueError):
+    """webtoon id is invalid. Or it can be adult webtoon, which is currently not supported."""
