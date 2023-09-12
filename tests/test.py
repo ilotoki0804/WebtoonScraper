369f47a77f9e1a7fabc8e4ed53ab66bd285181b4
ilotoki0804 <ilotoki0804@gmail.com>
2023-09-13 00:50:50 +0900

    절대 경로 import로 변경

--- tests/test.py
+++ tests/test.py
@@ -1,13 +1,8 @@
 import logging
 
-if __name__ in ("__main__", "tests"):
-    # 파일을 이용하는 것은 아님. 만약 제대로 사용하려면 상대 경로로 실행해야 함.
-    logging.warning(f'파일이 아닌 WebtoonScraper 모듈에서 실행되고 있습니다. {__name__ = }')
-    from WebtoonScraper import webtoon as wt
-    from WebtoonScraper import DirectoryMerger
-else:
-    from ..WebtoonScraper import webtoon as wt
-    from ..WebtoonScraper import DirectoryMerger
+from WebtoonScraper import webtoon as wt, DirectoryMerger
+
+logging.warning(f'파일이 아닌 WebtoonScraper 모듈에서 실행되고 있습니다. {__name__ = }')
 
 
 def skip_by_KeyboadInterrupt(func):
