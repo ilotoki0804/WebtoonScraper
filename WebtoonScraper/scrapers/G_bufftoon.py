298b5b22dcc2faea4407e0fb851bd6497b4b4e81
ilotoki0804 <ilotoki0804@gmail.com>
2023-09-13 00:50:39 +0900

    if-else 연산자로 변경

--- WebtoonScraper/scrapers/G_bufftoon.py
+++ WebtoonScraper/scrapers/G_bufftoon.py
@@ -49,10 +49,7 @@ class BufftoonScraper(Scraper[int]):
             'Upgrade-Insecure-Requests': '1',
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36 Edg/114.0.1823.43',
         }
-        if cookie is not None:
-            self.cookie: str = cookie
-        else:
-            self.cookie = ''
+        self.cookie = cookie if cookie is not None else ''
         self.update_requests()
 
     @override
