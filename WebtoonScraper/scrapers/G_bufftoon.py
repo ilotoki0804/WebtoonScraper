711aa97eeaf4827bf6e11707b799b0fade6de077
ilotoki0804 <ilotoki0804@gmail.com>
2023-11-22 21:45:36 +0900

    avoid_sslerror 추가
    Python 3.12부터 UNSAFE_LEGACY_RENEGOTIATION_DISABLED이 기본적으로 True로 설정되며
    버프툰을 제대로 불러오지 않는 오류가 발생했고,
    최근 버전의 requests_utils에서 이를 수정할 수 있는 방법을 만들어 이를 사용하도록 변경함.

--- WebtoonScraper/scrapers/G_bufftoon.py
+++ WebtoonScraper/scrapers/G_bufftoon.py
@@ -47,7 +47,7 @@ class BufftoonScraper(Scraper[int]):
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36 Edg/114.0.1823.43',
         }
         self.cookie = cookie if cookie is not None else ''
-        self.update_requests()
+        self.update_requests(avoid_sslerror=True)
 
     @reload_manager
     def fetch_episode_informations(
