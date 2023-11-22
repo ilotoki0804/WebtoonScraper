453b2538ecce2db987c3c08586dd06a4a05006f3
ilotoki0804 <ilotoki0804@gmail.com>
2023-11-22 22:57:19 +0900

    2.3.0 릴리즈

--- WebtoonScraper/__init__.py
+++ WebtoonScraper/__init__.py
@@ -15,7 +15,7 @@ __title__ = "WebtoonScraper"
 __description__ = "Scraping webtoons with ease."
 __url__ = "https://github.com/ilotoki0804/WebtoonScraper"
 __raw_source_url__ = "https://raw.githubusercontent.com/ilotoki0804/WebtoonScraper/master"
-__version_info__ = (2, 2, 0)
+__version_info__ = (2, 3, 0)
 __version__ = str.join('.', map(str, __version_info__))
 __author__ = "ilotoki0804"
 __author_email__ = "ilotoki0804@gmail.com"
@@ -26,7 +26,7 @@ if _sys.version_info < (3, 10):
     import logging
     logging.warning(f'Python version ({_sys.version}) is too low. Program may be run but not tested. '
                     'Upgrade Python if program not works well.')
-if _sys.version_info[:3] == (3, 12, 0):
+if _sys.version_info >= (3, 12):
     import logging
     logging.warning('This program works on Python 3.12.0, but it hurts performance. '
                     'Use python 3.11(3.11.4 is recommended.) or 3.10.')
