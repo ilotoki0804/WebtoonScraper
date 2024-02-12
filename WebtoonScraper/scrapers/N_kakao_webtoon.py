ec8dd6cf82b3d172ff63f184eaf172484f5119b0
ilotoki0804 <ilotoki0804@gmail.com>
2024-02-13 01:29:08 +0900

    코드 개선

--- WebtoonScraper/scrapers/N_kakao_webtoon.py
+++ WebtoonScraper/scrapers/N_kakao_webtoon.py
@@ -36,31 +36,11 @@ class KakaoWebtoonScraper(Scraper[int]):
         super().__init__(webtoon_id)
 
         self.client_id = int(random.random() * 2**32)
-        self.timestamp = int(time.time() * 1000)  # time.time을 사용할 수도 있음.
+        self.timestamp = int(time.time() * 1000)
         chars = [*range(0x30, 0x3A), *range(0x61, 0x7B)]
         self.nonce = "".join(chr(i) for i in random.choices(chars, k=10))
         self.app_id = f"KP.{self.client_id}.{self.timestamp + 1}"
 
-        self.post_headers = {
-            "Accept": "application/json, text/plain, */*",
-            "Accept-Encoding": "gzip, deflate, br",
-            "Accept-Language": "ko",
-            "Cache-Control": "no-cache",
-            "Content-Type": "application/json;charset=UTF-8",
-            "Cookie": f"theme=dark; _kp_collector={self.app_id}",
-            "Dnt": "1",
-            "Origin": "https://webtoon.kakao.com",
-            "Pragma": "no-cache",
-            "Referer": "https://webtoon.kakao.com/",
-            "Sec-Ch-Ua": '"Not A(Brand";v="99", "Microsoft Edge";v="121", "Chromium";v="121"',
-            "Sec-Ch-Ua-Mobile": "?0",
-            "Sec-Ch-Ua-Platform": '"Windows"',
-            "Sec-Fetch-Dest": "empty",
-            "Sec-Fetch-Mode": "cors",
-            "Sec-Fetch-Site": "same-site",
-            "Sec-Gpc": "1",
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36",
-        }
         self.episode_headers = {
             "Accept": "application/json, text/plain, */*",
             "Accept-Encoding": "gzip, deflate, br",
@@ -80,6 +60,7 @@ class KakaoWebtoonScraper(Scraper[int]):
             "Sec-Gpc": "1",
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36",
         }
+        self.post_headers = self.episode_headers | {"Content-Type": "application/json;charset=UTF-8"}
 
     @reload_manager
     def fetch_webtoon_information(self, *, reload: bool = False) -> None:
@@ -122,9 +103,10 @@ class KakaoWebtoonScraper(Scraper[int]):
                 raise InvalidWebtoonIdError(
                     f"Webtoon ID {self.webtoon_id} is invalid for Kakao Webtoon."
                 )
-            webtoon_episodes_data += res.json()["data"]["episodes"]
+            data = res.json()["data"]
+            webtoon_episodes_data += data["episodes"]
             offset += limit
-            is_last = res.json()["meta"]["pagination"]["last"]
+            is_last = data["pagination"]["last"]
 
         episode_ids: list[int] = []
         seo_ids: list[str] = []
@@ -190,12 +172,6 @@ class KakaoWebtoonScraper(Scraper[int]):
 
         return [(i["url"], key, iv) for i in data["media"]["files"]]
 
-    # async def async_download_webtoon(self, episode_no_range: EpisodeNoRange = None, merge_number: int | None = None, add_viewer: bool = True) -> None:
-    #     self.fetch_all()
-    #     unreadable_episodes = itertools.compress(self.episode_titles, (not is_readable for is_readable in self.readablities))
-    #     logger.warning(f"Unreadable episodes will be skipped: {', '.join(unreadable_episodes)}")
-    #     return await super().async_download_webtoon(episode_no_range, merge_number, add_viewer)
-
     @staticmethod
     def _decrypt(data: bytes, key: bytes, iv: bytes) -> bytes:
         cipher = AES.new(key, AES.MODE_CBC, iv)
@@ -207,11 +183,9 @@ class KakaoWebtoonScraper(Scraper[int]):
         user_id = episode_id
 
         temp_key = hashlib.sha256(
-            bytes(f"{user_id}{episode_id}{self.timestamp}", encoding="utf-8")
+            f"{user_id}{episode_id}{self.timestamp}".encode()
         ).digest()
-        temp_iv = hashlib.sha256(
-            bytes(f"{self.nonce}{self.timestamp}", encoding="utf-8")
-        ).digest()[:16]
+        temp_iv = hashlib.sha256(f"{self.nonce}{self.timestamp}".encode()).digest()[:16]
         encrypted_key = base64.b64decode(aid)
         encrypted_iv = base64.b64decode(zid)
 
