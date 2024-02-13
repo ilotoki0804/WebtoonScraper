1e87c6fb0bf509976461b1fcffe3c601020e1f0a
ilotoki0804 <ilotoki0804@gmail.com>
2024-02-14 01:58:37 +0900

    오류 처리 관련 개선

--- pyinstaller.py
+++ pyinstaller.py
@@ -9,11 +9,15 @@ print()
 print("Welcome to WebtoonScraper shell!")
 print("Type 'exit' to quit")
 while True:
+    # EOFError를 놔두기 위해 input은 suppress로 감싸지 않음.
     command = input(">>> ")
     if command == "exit":
         break
-    args = shlex.split(command)
-    if args[0].lower() in {"webtoon", "webtoonscraper"}:
-        del args[0]
+
     with suppress(BaseException):
+        # 잘못된 구문을 만났을 경우에도 튕기지 않기 위해
+        # shlex.split도 suppress로 감쌈.
+        args = shlex.split(command)
+        if args[0].lower() in {"webtoon", "webtoonscraper"}:
+            del args[0]
         main(args)
