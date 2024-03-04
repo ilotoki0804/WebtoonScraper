9d6cdde53f3de6e80ceab0d61337cd759afb6d49
ilotoki0804 <ilotoki0804@gmail.com>
2024-03-04 14:22:04 +0900

    Add main check for lezhin unshuffler

--- pyinstaller.py
+++ pyinstaller.py
@@ -3,21 +3,22 @@ import shlex
 
 from WebtoonScraper.__main__ import main
 
-with suppress(BaseException):
-    main(["--help"])
-print()
-print("Welcome to WebtoonScraper shell!")
-print("Type 'exit' to quit")
-while True:
-    # EOFError를 놔두기 위해 input은 suppress로 감싸지 않음.
-    command = input(">>> ")
-    if command == "exit":
-        break
-
+if __name__ == "__main__":
     with suppress(BaseException):
-        # 잘못된 구문을 만났을 경우에도 튕기지 않기 위해
-        # shlex.split도 suppress로 감쌈.
-        args = shlex.split(command)
-        if args[0].lower() in {"webtoon", "webtoonscraper"}:
-            del args[0]
-        main(args)
+        main(["--help"])
+    print()
+    print("Welcome to WebtoonScraper shell!")
+    print("Type 'exit' to quit")
+    while True:
+        # EOFError를 놔두기 위해 input은 suppress로 감싸지 않음.
+        command = input(">>> ")
+        if command == "exit":
+            break
+
+        with suppress(BaseException):
+            # 잘못된 구문을 만났을 경우에도 튕기지 않기 위해
+            # shlex.split도 suppress로 감쌈.
+            args = shlex.split(command)
+            if args[0].lower() in {"webtoon", "webtoonscraper"}:
+                del args[0]
+            main(args)
