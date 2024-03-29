c711c079fb12ea07a8179b4aa725865b6cee00d7
ilotoki0804 <ilotoki0804@gmail.com>
2024-03-29 23:49:32 +0900

    Revamp pyinstaller.yml as release.yml

--- build.py
+++ build.py
@@ -1,8 +1,5 @@
-"""Build a package that works on PyPI.
-
-Replace relative links with GitHub links and add warning 
-
-This program is not meant to be used as module.
+"""
+Replace relative path links with GitHub links and add warning in front of the long description.
 
 Last modified at 2023-03-26; 7th edition.
 """
@@ -38,31 +35,36 @@ def make_relative_link_work(match: re.Match) -> str:
     )
 
 
-try:
-    shutil.rmtree('dist')
-except FileNotFoundError:
-    os.mkdir('dist')
+def main():
+    try:
+        shutil.rmtree('dist')
+    except FileNotFoundError:
+        os.mkdir('dist')
 
-# update pyproject.toml version
-pyproject_path = Path("pyproject.toml")
-pyproject_data = tomlkit.parse(pyproject_path.read_text())
-pyproject_data['tool']['poetry']['version'] = __version__  # type: ignore
-pyproject_path.write_text(tomlkit.dumps(pyproject_data), encoding='utf-8')
+    # update pyproject.toml version
+    pyproject_path = Path("pyproject.toml")
+    pyproject_data = tomlkit.parse(pyproject_path.read_text())
+    pyproject_data['tool']['poetry']['version'] = __version__  # type: ignore
+    pyproject_path.write_text(tomlkit.dumps(pyproject_data), encoding='utf-8')
 
-long_description = f'Check lastest version [here]({github_project_url}).\n'
-long_description += Path('README.md').read_text(encoding='utf-8')
-long_description = re.sub(r'\[(?P<description>.*?)\]\((..\/)*(?P<path>(?P<directory_type>images|docs).*?)\)',
-                          make_relative_link_work, long_description)
+    long_description = f'**Check lastest version [here]({github_project_url}).**\n'
+    long_description += Path('README.md').read_text(encoding='utf-8')
+    long_description = re.sub(r'\[(?P<description>.*?)\]\((..\/)*(?P<path>(?P<directory_type>images|docs).*?)\)',
+                            make_relative_link_work, long_description)
 
-try:
-    Path("README_build.md").write_text(long_description, encoding='utf-8')
+    try:
+        Path("README_build.md").write_text(long_description, encoding='utf-8')
 
-    os.system('poetry build')
-    if PUBLISH:
-        if "PYPI_TOKEN" not in os.environ:
-            raise ValueError("Environment variable `PYPI_TOKEN` is not defined. Please set it.")
+        os.system('poetry build')
+        if PUBLISH:
+            if "PYPI_TOKEN" not in os.environ:
+                raise ValueError("Environment variable `PYPI_TOKEN` is not set.")
 
-        os.system(f'poetry publish -u __token__ -p {os.environ["PYPI_TOKEN"]}')
-finally:
-    if not LEAVE_README_BUILD_VERSION:
-        os.remove("README_build.md")
+            os.system(f'poetry publish -u __token__ -p {os.environ["PYPI_TOKEN"]}')
+    finally:
+        if not LEAVE_README_BUILD_VERSION:
+            os.remove("README_build.md")
+
+
+if __name__ == "__main__":
+    main()
