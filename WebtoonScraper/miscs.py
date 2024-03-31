f6a1ab69ac1cb52ccddc15815828383aa2c4b328
ilotoki0804 <ilotoki0804@gmail.com>
2024-03-31 18:33:33 +0900

    Use rich to display logs

--- WebtoonScraper/miscs.py
+++ WebtoonScraper/miscs.py
@@ -2,6 +2,8 @@ import contextlib
 import logging
 from typing import Any, Callable, Generic, Iterable, TypeAlias, TypeVar
 
+from rich.logging import RichHandler
+
 __github_project_name__ = __title__ = "WebtoonScraper"
 __github_user_name__ = __author__ = "ilotoki0804"
 __description__ = "Scraping webtoons with ease."
@@ -15,4 +17,5 @@ WebtoonId: TypeAlias = (
 EpisodeNoRange: TypeAlias = "tuple[int | None, int | None] | int | None | Iterable[int] | slice"
 
 logger = logging.getLogger("webtoonscraper_logger")
-# coloredlogs.install(logger=logger, datefmt='%H:%M:%S')
+logger.handlers = [RichHandler()]
+logger.setLevel(logging.INFO)
