9bb201e522105d517c441f413c8beb99c7ebf770
ilotoki0804 <ilotoki0804@gmail.com>
2024-06-22 10:18:19 +0900

    Release 0.4.0b1

--- WebtoonScraper/miscs.py
+++ WebtoonScraper/miscs.py
@@ -4,8 +4,8 @@ from typing import TYPE_CHECKING, Iterable, TypeAlias
 from rich.logging import RichHandler
 
 __url__ = "https://github.com/ilotoki0804/WebtoonScraper"
-__version_info__ = (3, 3, 1)
-__version__ = str.join(".", map(str, __version_info__))
+__version_info__ = (3, 4, 0, "b1")
+__version__ = str.join(".", map(str, __version_info__[:3])) + "".join(map(str, __version_info__[3:]))
 
 if TYPE_CHECKING:
     from .scrapers import NaverBlogWebtoonId as _NaverBlogWebtoonId
