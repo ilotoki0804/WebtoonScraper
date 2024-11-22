4b170fe8dd986e95a7d0516e5b186133953679a7
ilotoki0804 <ilotoki0804@gmail.com>
2024-11-23 00:10:52 +0900

    Release 5.0.0.post1

--- WebtoonScraper/base.py
+++ WebtoonScraper/base.py
@@ -12,7 +12,7 @@ from rich.console import Console
 if TYPE_CHECKING:
     from WebtoonScraper.scrapers import Scraper
 
-__version__ = "5.0.0"
+__version__ = "5.0.0.post1"
 
 
 def get_default_thread_number() -> int:
