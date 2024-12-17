bab59545670db7aff3f99e8e2b753cf7b99a1a5b
ilotoki0804 <ilotoki0804@gmail.com>
2024-12-18 02:26:23 +0900

    asyncio.run으로 인한 오류일 경우에만 관련 note를 붙임

--- WebtoonScraper/scrapers/_scraper.py
+++ WebtoonScraper/scrapers/_scraper.py
@@ -313,7 +313,8 @@ class Scraper(Generic[WebtoonId]):  # MARK: SCRAPER
         try:
             asyncio.run(self.async_download_webtoon(download_range=download_range))
         except RuntimeError as exc:
-            exc.add_note("Use `scraper.async_download_webtoon` in Jupyter or asyncio environment.")
+            if "event loop" in exc.args[0]:
+                exc.add_note("Use `scraper.async_download_webtoon` in Jupyter or asyncio environment.")
             raise
 
     async def async_download_webtoon(self, download_range: RangeType = None) -> None:
