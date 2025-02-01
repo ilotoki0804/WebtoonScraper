fc5f1bdeb8064343feb7578216b8302437a9400f
ilotoki0804 <ilotoki0804@gmail.com>
2025-02-01 10:04:23 +0900

    unshuffler가 warning 대신 info를 이용해 메시지를 보내도록 설정

--- WebtoonScraper/scrapers/_lezhin_unshuffler.py
+++ WebtoonScraper/scrapers/_lezhin_unshuffler.py
@@ -81,7 +81,7 @@ def unshuffle(
 
         unshuffle_parameters.append((source_episode_directory, target_episode_directory, episode_id))
 
-    logger.warning(
+    logger.info(
         "The webtoon is being unshuffled. It takes a while and it's very CPU-intensive task. "
         "So keep patient and wait until the process end."
     )
