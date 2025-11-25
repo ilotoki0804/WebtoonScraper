94692aaf8f8c09c722339e4fde7f92f0aa32b334
ilotoki0804 <ilotoki0804@gmail.com>
2025-11-25 13:40:26 +0900

    Release 5.10.1

--- WebtoonScraper/base.py
+++ WebtoonScraper/base.py
@@ -12,7 +12,7 @@ from rich.logging import RichHandler
 if TYPE_CHECKING:
     from WebtoonScraper.scrapers import Scraper
 
-__version__ = "5.10.0.post1"
+__version__ = "5.10.1"
 platforms: dict[str, type[Scraper]] = {}
 console = Console()
 
